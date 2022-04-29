025a9e073f5c20c818b823865e708ab76ed5a483
ARandomUserN <aforusinski@gmai.com>
2022-04-29 10:23:59 +0200

    Fix for MArks queries

--- src/main/java/com/example/journal/repositories/MarkRepository.java
+++ src/main/java/com/example/journal/repositories/MarkRepository.java
@@ -9,11 +9,11 @@ import com.example.journal.entities.Mark;
 
 public interface MarkRepository extends JpaRepository<Mark, Long>{
 	//All marks of student
-	@Query("SELECT m FROM Mark WHERE m.studentId = ?1")
+	@Query("SELECT m FROM Mark m WHERE m.studentId = ?1")
 	List<Mark> findMarks(Long studentId);
 	
 	//Marks of student from a subject
-	@Query("SELECT m FROM Mark WHERE m.studentId = ?1 AND m.studentId = ?2")
+	@Query("SELECT m FROM Mark m WHERE m.studentId = ?1 AND m.studentId = ?2")
 	List<Mark> findMarksByStudentSubject(Long studentId,Long subjectId);
 	
 	
